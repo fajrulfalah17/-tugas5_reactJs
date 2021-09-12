7c13e5b8737fdb275992aa2b82bcfaad9dfc0c86
fajrulfalah17 <fajruljunior45@gmail.com>
2021-09-12 21:49:52 +0700

    Tugas 5 ReactJs
    Tugas siap dikumpulkan

---
+++ tugas_5_react_js/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuMakanan from "./Page/MenuMakanan";
+
+function App() {
+  return (
+    <div className="App">
+      <MenuMakanan />
+    </div>
+  );
+}
+
+export default App;
